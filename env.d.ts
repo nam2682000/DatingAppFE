63d20928026ebab562bd18facc28b265e728c0a6
NamDV <namdv@rabiloo.com>
2024-10-31 14:33:45 +0700

    fix url

--- env.d.ts
+++ env.d.ts
@@ -1,18 +1,2 @@
 // src/env.d.ts
-declare module '*.env' {
-  const env: {
-    VITE_API_URL: string
-  }
-  export default env
-}
-
-interface ImportMetaEnv {
-  readonly VITE_API_URL: string
-  readonly VITE_APP_TITLE: string
-  readonly VITE_BASE_URL: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
 /// <reference types="vite/client" />
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import { useAuthStore } from '../stores/authStore'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
